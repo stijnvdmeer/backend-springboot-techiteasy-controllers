a6e99b56bd2630f2c7742fa67b58fabd084a53f3
stijnvdmeer <stijn08072002@outlook.com>
2025-02-05 09:44:54 +0100

    initial commit 2

---
+++ src/main/java/org/example/backendspringtechiteasycontroller/BackendSpringTechiteasyControllerApplication.java
@@ -0,0 +1,13 @@
+package org.example.backendspringtechiteasycontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSpringTechiteasyControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendSpringTechiteasyControllerApplication.class, args);
+    }
+
+}
