adc060006f4041aa3b8dfe2615f68ed16c6ae93b
stijnvdmeer <stijn08072002@outlook.com>
2025-02-05 09:53:09 +0100

    Created Television Class

---
+++ src/main/java/org/example/backendspringtechiteasycontroller/models/Television.java
@@ -0,0 +1,39 @@
+package org.example.backendspringtechiteasycontroller.models;
+
+public class Television {
+    private static int id;
+    private String brand;
+    private String model;
+    private double price;
+
+    public Television(String brand, String model, double price) {
+        this.id = id++;
+        this.brand = brand;
+        this.model = model;
+        this.price = price;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
